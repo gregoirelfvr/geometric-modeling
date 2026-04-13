6cf637c97a6f0714f9862584e36f331025ac3db5
Grégoire <gregoirelefevre26@gmail.com>
2026-04-13 15:46:44 +0200

    computeNormals()

--- MeshViewerCMake/myFace.cpp
+++ MeshViewerCMake/myFace.cpp
@@ -17,17 +17,5 @@ myFace::~myFace(void)
 
 void myFace::computeNormal()
 {
-	myHalfedge *h = adjacent_halfedge;
-	myVector3D v1, v2;
-	v1.dX = h->source->point->X - h->next->source->point->X;
-	v1.dY = h->source->point->Y - h->next->source->point->Y;
-	v1.dZ = h->source->point->Z - h->next->source->point->Z;
-
-	h = h->next;
-	v2.dX = h->source->point->X - h->next->source->point->X;
-	v2.dY = h->source->point->Y - h->next->source->point->Y;
-	v2.dZ = h->source->point->Z - h->next->source->point->Z;
-
-	normal->crossproduct(v1, v2);
-	normal->normalize();
+	normal->setNormal(adjacent_halfedge->source->point, adjacent_halfedge->next->source->point, adjacent_halfedge->next->next->source->point);
 }
--- MeshViewerCMake/myMesh.cpp
+++ MeshViewerCMake/myMesh.cpp
@@ -127,7 +127,14 @@ bool myMesh::readFile(std::string filename) {
   return true;
 }
 
-void myMesh::computeNormals() { /**** TODO ****/ }
+void myMesh::computeNormals() { 
+  for (unsigned int i = 0; i < faces.size(); i++)
+    faces[i]->computeNormal();
+
+  for (unsigned int i = 0; i < vertices.size(); i++) {
+    vertices[i]->computeNormal();
+  }
+ }
 
 void myMesh::normalize() {
   if (vertices.size() < 1)
